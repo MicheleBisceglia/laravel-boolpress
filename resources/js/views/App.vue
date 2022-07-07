7a2d8ff0d321c2e4401585c9d3382e85b9fddfb9
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-07-07 23:55:51 +0200

    vue configuration

---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Laooo, ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati iusto eaque magnam quisquam, est, neque ipsam possimus nisi temporibus dolorum cumque exercitationem, laborum harum asperiores tenetur consequuntur quo adipisci.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,1 +1,7 @@
 <h1>Guest Home</h1>
+
+<div id="root">
+  
+</div>
+
+<script src=" {{ asset('js/front.js')}}"></script>
